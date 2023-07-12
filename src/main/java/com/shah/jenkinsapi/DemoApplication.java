038d8e10d1827b38929ae42b1034e2e9d2fcfb24
shahjavadeveloper <shahjavadeveloper@gmail.com>
2023-07-12 16:35:31 +0530

    added endpoint

--- src/main/java/com/shah/jenkinsapi/DemoApplication.java
+++ src/main/java/com/shah/jenkinsapi/DemoApplication.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -16,6 +17,11 @@ public class DemoApplication {
 	public void init() {
 		log.info("Application started.......");
 	}
+
+   @GetMapping("/message")
+   public String getMessage() {
+	   return "welcome to docker application";
+   }
 
 	public static void main(String[] args) {
 		log.info("Application executed");
